88133fbf6ff9bfbcb7181727ffeb88eb3e9e9fba
Cranche <445343@mail.muni.cz>
2018-07-20 13:30:54 +0200

    tests for TrainingRunService

--- kypo2-service-training/src/test/java/cz/muni/ics/kypo/service/TrainingRunServiceTest.java
+++ kypo2-service-training/src/test/java/cz/muni/ics/kypo/service/TrainingRunServiceTest.java
@@ -1,9 +1,12 @@
 package cz.muni.ics.kypo.service;
 
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.PathBuilder;
 import cz.muni.ics.kypo.exceptions.ServiceLayerException;
 import cz.muni.ics.kypo.model.TrainingRun;
 import cz.muni.ics.kypo.model.enums.TRState;
 import cz.muni.ics.kypo.repository.TrainingRunRepository;
+import org.hibernate.HibernateException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -15,9 +18,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
@@ -38,7 +47,7 @@ public class TrainingRunServiceTest {
     @MockBean
     private TrainingRunRepository trainingRunRepository;
 
-    private TrainingRun trainingRun1;
+    private TrainingRun trainingRun1, trainingRun2;
 
     @SpringBootApplication
     static class TestConfiguration {
@@ -50,10 +59,13 @@ public class TrainingRunServiceTest {
         trainingRun1.setId(1L);
         trainingRun1.setState(TRState.NEW);
 
+        trainingRun2 = new TrainingRun();
+        trainingRun2.setId(2L);
+        trainingRun2.setState(TRState.ARCHIVED);
     }
 
     @Test
-    public void getTrainingRun() {
+    public void getTrainingRunById() {
         given(trainingRunRepository.findById(trainingRun1.getId())).willReturn(Optional.of(trainingRun1));
 
         TrainingRun t = trainingRunService.findById(trainingRun1.getId()).get();
@@ -61,14 +73,36 @@ public class TrainingRunServiceTest {
         assertEquals(t.getState(), trainingRun1.getState());
 
         then(trainingRunRepository).should().findById(trainingRun1.getId());
-
     }
 
     @Test
-    public void getTrainingRunWithNullId() {
-        Long id = null;
+    public void getTrainingRunByIdWithHibernateException() {
+        Long id = 6L;
+        willThrow(HibernateException.class).given(trainingRunRepository).findById(id);
         thrown.expect(ServiceLayerException.class);
         trainingRunService.findById(id);
+    }
+
+    @Test
+    public void findNonexistingTrainingRunById() {
+        Long id = 6L;
+        assertEquals(Optional.empty(), trainingRunService.findById(id));
+    }
+
+    @Test
+    public void findAll() {
+        List<TrainingRun> expected = new ArrayList<>();
+        expected.add(trainingRun1);
+        expected.add(trainingRun2);
+
+        Page p = new PageImpl<TrainingRun>(expected);
+        PathBuilder<TrainingRun> t = new PathBuilder<TrainingRun>(TrainingRun.class, "trainingRun");
+        Predicate predicate = t.isNotNull();
+
+        given(trainingRunRepository.findAll(any(Predicate.class), any(Pageable.class))).willReturn(p);
+
+        Page pr = trainingRunService.findAll(predicate, PageRequest.of(0,2));
+        assertEquals(2, pr.getTotalElements());
     }
 
     @After
