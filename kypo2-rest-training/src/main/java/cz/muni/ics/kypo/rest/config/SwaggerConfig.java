29bef65fb052a1d4817412ab4ccc61c770bfb22a
Dominik Pilar <445537@mail.muni.cz>
2018-07-22 21:50:10 +0200

    Rebased with master

--- kypo2-rest-training/src/main/java/cz/muni/ics/kypo/rest/config/SwaggerConfig.java
+++ kypo2-rest-training/src/main/java/cz/muni/ics/kypo/rest/config/SwaggerConfig.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.builders.*;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
