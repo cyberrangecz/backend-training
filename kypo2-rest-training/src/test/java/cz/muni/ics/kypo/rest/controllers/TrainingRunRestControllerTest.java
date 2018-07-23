fbda8505cef8829f99b153eaf3db48816618c613
Cranche <445343@mail.muni.cz>
2018-07-23 16:59:28 +0200

    getters and setters added to DTO classes

--- kypo2-rest-training/src/test/java/cz/muni/ics/kypo/rest/controllers/TrainingRunRestControllerTest.java
+++ kypo2-rest-training/src/test/java/cz/muni/ics/kypo/rest/controllers/TrainingRunRestControllerTest.java
@@ -2,8 +2,11 @@ package cz.muni.ics.kypo.rest.controllers;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import cz.muni.ics.kypo.api.PageResultResource;
 import cz.muni.ics.kypo.api.dto.TrainingRunDTO;
 import cz.muni.ics.kypo.mapping.BeanMapping;
+import cz.muni.ics.kypo.mapping.BeanMappingImpl;
 import cz.muni.ics.kypo.model.TrainingRun;
 import cz.muni.ics.kypo.model.enums.TRState;
 import cz.muni.ics.kypo.service.TrainingRunService;
@@ -13,7 +16,12 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
@@ -29,19 +37,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 
 @RunWith(SpringRunner.class)
 public class TrainingRunRestControllerTest {
 
-    @Mock
+    @MockBean
     private TrainingRunService trainingRunService;
 
-    @Mock
+    @MockBean
     private BeanMapping beanMapping;
 
-    @InjectMocks
+    @MockBean
+    @Qualifier("objMapperRESTApi")
+    private ObjectMapper objectMapper;
+
+
+    @Autowired
     private TrainingRunsRestController trainingRunsRestController;
 
     private MockMvc mockMvc;
@@ -52,11 +67,13 @@ public class TrainingRunRestControllerTest {
 
     private TrainingRunDTO trainingRun1DTO, trainingRun2DTO;
 
+    private PageResultResource<TrainingRunDTO> trainingRunDTOPageResultResource;
+
+    private Page p;
+
     @Before
     public void init() {
-        pageable = PageRequest.of(0, 10);
         MockitoAnnotations.initMocks(this);
-
         this.mockMvc = MockMvcBuilders.standaloneSetup(trainingRunsRestController)
                 .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
                 .setMessageConverters(new MappingJackson2HttpMessageConverter()).build();
@@ -70,22 +87,48 @@ public class TrainingRunRestControllerTest {
         trainingRun2.setState(TRState.ARCHIVED);
 
         trainingRun1DTO = new TrainingRunDTO();
+        trainingRun1DTO.setId(1L);
+        trainingRun1DTO.setState(TRState.NEW);
+
+        trainingRun2DTO = new TrainingRunDTO();
+        trainingRun2DTO.setId(2L);
+        trainingRun2DTO.setState(TRState.ARCHIVED);
+
+        List<TrainingRun> expected = new ArrayList();
+        expected.add(trainingRun1);
+        expected.add(trainingRun2);
+
+        p = new PageImpl<TrainingRun>(expected);
+
+        ObjectMapper obj = new ObjectMapper();
+        obj.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        given(objectMapper.getSerializationConfig()).willReturn(obj.getSerializationConfig());
+
+        BeanMapping bM = new BeanMappingImpl(new ModelMapper());
+        trainingRunDTOPageResultResource = bM.mapToPageResultDTO(p, TrainingRunDTO.class);
 
 
-        given(beanMapping.mapTo(trainingRun1, TrainingRunDTO.class)).willReturn(trainingRun1DTO);
-        given(beanMapping.mapTo(trainingRun2, TrainingRunDTO.class)).willReturn(trainingRun2DTO);
     }
 
     @Test
     public void findTrainingRunById() throws Exception {
         given(trainingRunService.findById(trainingRun1.getId())).willReturn(Optional.of(trainingRun1));
+        String valueTr = convertObjectToJsonBytes(trainingRun1DTO);
+        given(objectMapper.writeValueAsString(any(Object.class))).willReturn(valueTr);
 
         mockMvc.perform(get("/training-runs/{id}", trainingRun1.getId()))
             .andExpect(status().isOk())
             .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
             .andExpect(content().string(convertObjectToJsonBytes(trainingRun1DTO)));
     }
-
+    /*
+    @Test
+    public void findAllTrainingRuns() throws Exception {
+        String valueTr = convertObjectToJsonBytes(trainingRunDTOPageResultResource);
+        given(objectMapper.writeValueAsString(any(Object.class))).willReturn(valueTr);
+        given(trainingRunFacade)
+    }
+*/
     private static String convertObjectToJsonBytes(Object object) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(object);
