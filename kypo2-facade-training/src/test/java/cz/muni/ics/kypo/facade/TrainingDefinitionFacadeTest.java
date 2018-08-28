c0bd205f0fb006c718bda01a6db259d108d60ce3
Boris Jadus <445343@mail.muni.cz>
2018-08-28 11:12:38 +0200

    Tests for TrainingDefinitionFacade class

--- kypo2-facade-training/src/test/java/cz/muni/ics/kypo/facade/TrainingDefinitionFacadeTest.java
+++ kypo2-facade-training/src/test/java/cz/muni/ics/kypo/facade/TrainingDefinitionFacadeTest.java
@@ -6,10 +6,13 @@ import cz.muni.ics.kypo.api.PageResultResource;
 import cz.muni.ics.kypo.api.dto.TrainingDefinitionDTO;
 import cz.muni.ics.kypo.config.FacadeTestConfiguration;
 import cz.muni.ics.kypo.exception.FacadeLayerException;
+import cz.muni.ics.kypo.exceptions.CannotBeUpdatedException;
 import cz.muni.ics.kypo.exceptions.ServiceLayerException;
+import cz.muni.ics.kypo.model.AssessmentLevel;
 import cz.muni.ics.kypo.model.TrainingDefinition;
 import cz.muni.ics.kypo.model.enums.TDState;
 import cz.muni.ics.kypo.service.TrainingDefinitionService;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -29,11 +32,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.validation.constraints.Null;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.BDDMockito.*;
 
 @RunWith(SpringRunner.class)
@@ -53,7 +59,9 @@ public class TrainingDefinitionFacadeTest {
     @MockBean
     private TrainingDefinitionService trainingDefinitionService;
 
-    private TrainingDefinition trainingDefinition1, trainingDefinition2;
+    private TrainingDefinition trainingDefinition1, trainingDefinition2, unreleasedDefinition, releasedDefinition;
+
+    private AssessmentLevel level1;
 
     @SpringBootApplication
     static class TestConfiguration {
@@ -61,13 +69,26 @@ public class TrainingDefinitionFacadeTest {
 
     @Before
     public void init() {
+        level1 = new AssessmentLevel();
+        level1.setId(1L);
+
         trainingDefinition1 = new TrainingDefinition();
         trainingDefinition1.setId(1L);
-        trainingDefinition1.setState(TDState.ARCHIVED);
+        trainingDefinition1.setState(TDState.RELEASED);
 
         trainingDefinition2 = new TrainingDefinition();
         trainingDefinition2.setId(2L);
-        trainingDefinition2.setState(TDState.RELEASED);
+        trainingDefinition2.setState(TDState.ARCHIVED);
+
+        unreleasedDefinition = new TrainingDefinition();
+        unreleasedDefinition.setId(4L);
+        unreleasedDefinition.setState(TDState.UNRELEASED);
+        unreleasedDefinition.setStartingLevel(level1.getId());
+
+        releasedDefinition = new TrainingDefinition();
+        releasedDefinition.setState(TDState.RELEASED);
+        releasedDefinition.setId(5L);
+
     }
 
     @Test
@@ -108,15 +129,105 @@ public class TrainingDefinitionFacadeTest {
     }
 
     @Test
-    public void updateTrainingDefinitionWithNull() {
-      thrown.expect(FacadeLayerException.class);
-      trainingDefinitionFacade.update(null);
+    public void updateTrainingDefinition() {
+        trainingDefinitionFacade.update(unreleasedDefinition);
+        then(trainingDefinitionService).should().update(unreleasedDefinition);
     }
 
     @Test
-    public void cloneTrainingDefinitionWithNull(){
+    public void udateTrainingDefinitionWithNull() {
+        thrown.expect(FacadeLayerException.class);
+        trainingDefinitionFacade.update(null);
+    }
+
+    @Test
+    public void cloneTrainingDefinition() {
+        TrainingDefinition clonedDefinition = new TrainingDefinition();
+        clonedDefinition.setId(3L);
+        clonedDefinition.setState(TDState.UNRELEASED);
+        clonedDefinition.setTitle("Clone of " + trainingDefinition1.getTitle());
+
+        given(trainingDefinitionService.clone(trainingDefinition1.getId())).willReturn(Optional.of(clonedDefinition));
+
+        TrainingDefinitionDTO newClone = trainingDefinitionFacade.clone(trainingDefinition1.getId());
+        assertEquals("Clone of " + trainingDefinition1.getTitle(), newClone.getTitle());
+        assertNotEquals(trainingDefinition1.getState(), newClone.getState());
+        assertNotEquals(trainingDefinition1.getId(), newClone.getId());
+
+        then(trainingDefinitionService).should().clone(trainingDefinition1.getId());
+    }
+
+    @Test
+    public void cloneTrainingDefinitionWithNull() {
         thrown.expect(NullPointerException.class);
         trainingDefinitionFacade.clone(null);
+    }
+
+    @Test
+    public void swapLeft() {
+        trainingDefinitionFacade.swapLeft(unreleasedDefinition.getId(),level1.getId());
+        then(trainingDefinitionService).should().swapLeft(unreleasedDefinition.getId(), level1.getId());
+    }
+
+    @Test
+    public void swapLeftWithNullDefinition() {
+        thrown.expect(FacadeLayerException.class);
+        trainingDefinitionFacade.swapLeft(null, level1.getId());
+    }
+
+    @Test
+    public void swapLeftWithNullLevel() {
+        thrown.expect(FacadeLayerException.class);
+        trainingDefinitionFacade.swapLeft(releasedDefinition.getId(), null);
+    }
+
+
+    @Test
+    public void swapRight() {
+        trainingDefinitionFacade.swapRight(unreleasedDefinition.getId(),level1.getId());
+        then(trainingDefinitionService).should().swapRight(unreleasedDefinition.getId(), level1.getId());
+    }
+
+    @Test
+    public void swapRightWithNullDefinition() {
+        thrown.expect(FacadeLayerException.class);
+        trainingDefinitionFacade.swapRight(null, level1.getId());
+    }
+
+    @Test
+    public void swapRightWithNullLevel() {
+        thrown.expect(FacadeLayerException.class);
+        trainingDefinitionFacade.swapRight(releasedDefinition.getId(), null);
+    }
+
+    @Test
+    public void deleteTrainingDefinition() {
+        trainingDefinitionFacade.delete(trainingDefinition1.getId());
+        then(trainingDefinitionService).should().delete(trainingDefinition1.getId());
+    }
+
+    @Test
+    public void deleteTrainingDefinitionWithNull() {
+        thrown.expect(NullPointerException.class);
+        trainingDefinitionFacade.delete(null);
+    }
+
+    @Test
+    public void deleteOneLevel(){
+        trainingDefinitionFacade.deleteOneLevel(trainingDefinition1.getId(), level1.getId());
+        then(trainingDefinitionService).should().deleteOneLevel(trainingDefinition1.getId(), level1.getId());
+    }
+
+    @Test
+    public void deleteOneLevelWithNullDefinition() {
+        thrown.expect(NullPointerException.class);
+        trainingDefinitionFacade.deleteOneLevel(null, level1.getId());
+    }
+
+    @Test
+    public void deleteOneLevelWithNullLevel() {
+        thrown.expect(NullPointerException.class);
+        trainingDefinitionFacade.deleteOneLevel(trainingDefinition1.getId(), null);
     }
 
     private void deepEquals(TrainingDefinition expected, TrainingDefinitionDTO actual) {
