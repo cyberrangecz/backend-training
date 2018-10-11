d81be42fdd868b2a8c16a4271112d4ccee93bf51
456221 <456221@mail.muni.cz>
2018-10-11 16:05:43 +0200

    unused imports deleted

--- kypo2-persistence-training/src/test/java/cz/muni/ics/kypo/training/persistence/repository/TrainingRunRepositoryTest.java
+++ kypo2-persistence-training/src/test/java/cz/muni/ics/kypo/training/persistence/repository/TrainingRunRepositoryTest.java
@@ -6,14 +6,12 @@ import cz.muni.ics.kypo.training.persistence.config.PersistenceConfigTest;
 import cz.muni.ics.kypo.training.persistence.model.*;
 import cz.muni.ics.kypo.training.persistence.model.enums.TDState;
 import cz.muni.ics.kypo.training.persistence.model.enums.TRState;
-import cz.muni.ics.kypo.training.persistence.repository.TrainingRunRepository;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
