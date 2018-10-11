fba509a3e8ff09253f3414d6118ad766fe2a33eb
456221 <456221@mail.muni.cz>
2018-10-11 16:00:03 +0200

    findAll and findById tests added for Password repository

--- kypo2-persistence-training/src/test/java/cz/muni/ics/kypo/training/persistence/repository/PasswordRepositoryTest.java
+++ kypo2-persistence-training/src/test/java/cz/muni/ics/kypo/training/persistence/repository/PasswordRepositoryTest.java
@@ -1,18 +1,17 @@
-package cz.muni.ics.kypo.training.repository;
+package cz.muni.ics.kypo.training.persistence.repository;
 
-import cz.muni.ics.kypo.training.model.ParticipantRef;
-import cz.muni.ics.kypo.training.model.Password;
+import cz.muni.ics.kypo.training.persistence.config.PersistenceConfigTest;
+import cz.muni.ics.kypo.training.persistence.model.Password;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,7 +19,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@EntityScan(basePackages = {"cz.muni.ics.kypo.training.model"})
+@Import(PersistenceConfigTest.class)
 public class PasswordRepositoryTest {
 
 		@Autowired
