df4188650621e515852871456c7bff059301e8fd
456221 <456221@mail.muni.cz>
2018-10-15 10:57:41 +0200

    setted smaller indent

--- kypo2-persistence-training/src/test/java/cz/muni/ics/kypo/training/persistence/repository/GameLevelRepositoryTest.java
+++ kypo2-persistence-training/src/test/java/cz/muni/ics/kypo/training/persistence/repository/GameLevelRepositoryTest.java
@@ -25,66 +25,63 @@ import static org.junit.Assert.*;
 @Import(PersistenceConfigTest.class)
 public class GameLevelRepositoryTest {
 
-		@Autowired
-		private TestEntityManager entityManager;
+	@Autowired
+	private TestEntityManager entityManager;
 
-		@Autowired
-		private GameLevelRepository gameLevelRepository;
+	@Autowired
+	private GameLevelRepository gameLevelRepository;
 
-		private GameLevel gameLevel1, gameLevel2;
+	private GameLevel gameLevel1, gameLevel2;
 
-		@SpringBootApplication
-		static class TestConfiguration { }
+	@SpringBootApplication
+	static class TestConfiguration { }
 
-		@Before
-		public void setUp() {
-			gameLevel1 = new GameLevel();
-			gameLevel1.setFlag("flag1");
-			gameLevel1.setContent("content1");
-			gameLevel1.setSolution("solution1");
-			gameLevel1.setSolutionPenalized(true);
-			gameLevel1.setTitle("title1");
+	@Before
+	public void setUp() {
+		gameLevel1 = new GameLevel();
+		gameLevel1.setFlag("flag1");
+		gameLevel1.setContent("content1");
+		gameLevel1.setSolution("solution1");
+		gameLevel1.setSolutionPenalized(true);
+		gameLevel1.setTitle("title1");
+		gameLevel2 = new GameLevel();
+		gameLevel2.setFlag("flag2");
+		gameLevel2.setContent("content2");
+		gameLevel2.setSolution("solution2");
+		gameLevel2.setSolutionPenalized(true);
+		gameLevel2.setTitle("title2");
+	}
 
-			gameLevel2 = new GameLevel();
-			gameLevel2.setFlag("flag2");
-			gameLevel2.setContent("content2");
-			gameLevel2.setSolution("solution2");
-			gameLevel2.setSolutionPenalized(true);
-			gameLevel2.setTitle("title2");
-		}
+	@Test
+	public void findById() {
+		Long id = (Long) entityManager.persistAndGetId(gameLevel1);
+		Optional<GameLevel> optionalGameLevel = gameLevelRepository.findById(id);
+		assertTrue(optionalGameLevel.isPresent());
+		assertEquals(gameLevel1, optionalGameLevel.get());
+	}
 
-		@Test
-		public void findById() {
-			Long id = (Long) entityManager.persistAndGetId(gameLevel1);
-			Optional<GameLevel> optionalGameLevel = gameLevelRepository.findById(id);
-			assertTrue(optionalGameLevel.isPresent());
-			assertEquals(gameLevel1, optionalGameLevel.get());
-		}
+	@Test(expected = InvalidDataAccessApiUsageException.class)
+	public void findById_nullableArgument() throws Exception {
+			entityManager.persist(gameLevel2);
+			Optional<GameLevel> optionalGameLevel = gameLevelRepository.findById(null);
+	}
 
-		@Test(expected = InvalidDataAccessApiUsageException.class)
-		public void findById_nullableArgument() throws Exception {
-				entityManager.persist(gameLevel2);
-				Optional<GameLevel> optionalGameLevel = gameLevelRepository.findById(null);
-		}
+	@Test
+	public void findAll() {
+		List<GameLevel> expectedGameLevel = Arrays.asList(gameLevel1, gameLevel2);
+		expectedGameLevel.stream().forEach(g -> entityManager.persist(g));
+		List<GameLevel> resultGameLevel = gameLevelRepository.findAll();
+		assertNotNull(resultGameLevel);
+		assertEquals(expectedGameLevel, resultGameLevel);
+		assertEquals(2, resultGameLevel.size());
+	}
 
-		@Test
-		public void findAll() {
-			List<GameLevel> expectedGameLevel = Arrays.asList(gameLevel1, gameLevel2);
-
-			expectedGameLevel.stream().forEach(g -> entityManager.persist(g));
-
-			List<GameLevel> resultGameLevel = gameLevelRepository.findAll();
-			assertNotNull(resultGameLevel);
-			assertEquals(expectedGameLevel, resultGameLevel);
-			assertEquals(2, resultGameLevel.size());
-		}
-
-		@Test
-		public void findAll_emptyDatabase() {
-			List<GameLevel> expectedGameLevel = new ArrayList<>();
-			List<GameLevel> resultGameLevel = gameLevelRepository.findAll();
-			assertNotNull(resultGameLevel);
-			assertEquals(expectedGameLevel, resultGameLevel);
-			assertEquals(0, resultGameLevel.size());
-		}
+	@Test
+	public void findAll_emptyDatabase() {
+		List<GameLevel> expectedGameLevel = new ArrayList<>();
+		List<GameLevel> resultGameLevel = gameLevelRepository.findAll();
+		assertNotNull(resultGameLevel);
+		assertEquals(expectedGameLevel, resultGameLevel);
+		assertEquals(0, resultGameLevel.size());
+	}
 }
