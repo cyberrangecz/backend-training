7ef5dae44b0d8e6fc01f53fa5bf91d2081bb3c65
xhasak <456221@mail.muni.cz>
2018-12-04 22:04:58 +0100

    testing data for elasticsearch module added

---
+++ kypo2-elasticsearch-training/src/main/resources/testing-data/events/curl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+API_ROOT="http://localhost:9200/"
+TEMPLATE_INFO="_template/template_1"
+TEMPLATE_PATH="../template.json"
+
+curl -X PUT -d @${TEMPLATE_PATH} ${API_ROOT}${TEMPLATE_INFO}
+
+for DIR in */
+do
+  EVENT=${DIR:0:-1}
+  cd $DIR
+
+  for FILE in *.json
+  do
+    UPPERCASE="kypo2-cz.muni.csirt.kypo.events.trainings.${EVENT}"
+    LOWERCASE=${UPPERCASE,,}
+    curl -X POST -d @"$FILE" "${API_ROOT}${LOWERCASE}/$UPPERCASE"
+  done
+
+  cd ..
+done
