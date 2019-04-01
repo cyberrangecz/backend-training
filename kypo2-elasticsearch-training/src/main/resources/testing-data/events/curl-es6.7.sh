65baa576c5bd47ef0d221c5ef45efb213042d3d8
Šimon Hašák <456221@mail.muni.cz>
2019-04-01 12:27:25 +0000

    elasticsearch query for inserting template repair with JSON date format

--- kypo2-elasticsearch-training/src/main/resources/testing-data/events/curl-es6.7.sh
+++ kypo2-elasticsearch-training/src/main/resources/testing-data/events/curl-es6.7.sh
@@ -3,7 +3,7 @@ API_ROOT="http://localhost:9200/"
 TEMPLATE_INFO="_template/template_1"
 TEMPLATE_PATH="../template.json"
 
-curl -X PUT -d @${TEMPLATE_PATH} ${API_ROOT}${TEMPLATE_INFO}
+curl -X PUT -d @${TEMPLATE_PATH} ${API_ROOT}${TEMPLATE_INFO} -H 'Content-Type: application/json'
 
 for DIR in */
 do
