031793fe27c97a20d1330d07fe5ed54743a5f764
Boris Jaduš <445343@mail.muni.cz>
2019-04-03 14:01:19 +0200

    archive training instance endpoint created

--- kypo2-rest-training/src/main/java/cz/muni/ics/kypo/training/rest/controllers/ExportImportRestController.java
+++ kypo2-rest-training/src/main/java/cz/muni/ics/kypo/training/rest/controllers/ExportImportRestController.java
@@ -3,16 +3,19 @@ package cz.muni.ics.kypo.training.rest.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.bohnman.squiggly.Squiggly;
 import com.github.bohnman.squiggly.util.SquigglyUtils;
+import cz.muni.ics.kypo.training.api.dto.archive.TrainingInstanceArchiveDTO;
 import cz.muni.ics.kypo.training.api.dto.export.ExportTrainingDefinitionAndLevelsDTO;
 import cz.muni.ics.kypo.training.api.dto.imports.ImportTrainingDefinitionDTO;
 import cz.muni.ics.kypo.training.api.dto.trainingdefinition.TrainingDefinitionDTO;
 import cz.muni.ics.kypo.training.exceptions.FacadeLayerException;
 import cz.muni.ics.kypo.training.facade.ExportImportFacade;
+import cz.muni.ics.kypo.training.persistence.model.TrainingInstance;
 import cz.muni.ics.kypo.training.rest.exceptions.ResourceNotFoundException;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -88,6 +91,32 @@ public class ExportImportRestController {
         TrainingDefinitionDTO trainingDefinitionResource = exportImportFacade.dbImport(importTrainingDefinitionDTO);
         Squiggly.init(objectMapper, fields);
         return ResponseEntity.ok(SquigglyUtils.stringify(objectMapper, trainingDefinitionResource));
+    }
+
+    @ApiOperation(httpMethod = "GET",
+            value = "Archive training instance",
+            response = TrainingInstanceArchiveDTO.class,
+            nickname = "archiveTrainingInstance",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Training instance archived.", response = TrainingInstanceArchiveDTO.class),
+        @ApiResponse(code = 404, message = "Training instance not found."),
+        @ApiResponse(code = 500, message = "Unexpected condition was encountered.")
+
+    })
+    @GetMapping(path = "/exports/training-instance/{trainingInstanceId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> archiveTrainingInstance(
+            @ApiParam(value = "Id of training instance", required = true)
+            @PathVariable(value = "trainingInstanceId") Long trainingInstanceId,
+            @ApiParam(value = "Fields which should be returned in REST API response", required = false)
+            @RequestParam(value = "fields", required = false) String fields){
+        try{
+            Squiggly.init(objectMapper, fields);
+            return ResponseEntity.ok(SquigglyUtils.stringify(objectMapper, exportImportFacade.archiveTrainingInstance(trainingInstanceId)));
+        } catch (FacadeLayerException ex){
+            throw new ResourceNotFoundException(ex);
+        }
+
     }
 
 }
