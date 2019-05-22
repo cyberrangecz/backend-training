70e57fc74c5cb086ff3866cac6fe35a28efd1896
Boris Jaduš <445343@mail.muni.cz>
2019-05-22 15:37:26 +0200

    find instance by Id IT fixed after refactor

--- kypo2-rest-training/src/test/java/cz/muni/ics/kypo/training/rest/controllers/TrainingInstancesIT.java
+++ kypo2-rest-training/src/test/java/cz/muni/ics/kypo/training/rest/controllers/TrainingInstancesIT.java
@@ -130,6 +130,7 @@ public class TrainingInstancesIT {
 		definition.setBetaTestingGroup(betaTestingGroup);
 		definition.setSandboxDefinitionRefId(1L);
 		definition.setLastEdited(LocalDateTime.now());
+		definition.setAuthors(new HashSet<>(Arrays.asList(uR)));
 		TrainingDefinition tD = trainingDefinitionRepository.save(definition);
 
 		futureTrainingInstance = new TrainingInstance();
