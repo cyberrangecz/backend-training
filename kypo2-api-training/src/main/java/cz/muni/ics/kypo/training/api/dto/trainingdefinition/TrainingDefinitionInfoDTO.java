1354c600e3bcd86b8812fff70e11677cc9181976
Dominik <445537@mail.muni.cz>
2019-05-28 15:32:59 +0200

    State added to TD DTO for organizers

--- kypo2-api-training/src/main/java/cz/muni/ics/kypo/training/api/dto/trainingdefinition/TrainingDefinitionInfoDTO.java
+++ kypo2-api-training/src/main/java/cz/muni/ics/kypo/training/api/dto/trainingdefinition/TrainingDefinitionInfoDTO.java
@@ -1,11 +1,15 @@
 package cz.muni.ics.kypo.training.api.dto.trainingdefinition;
 
+import cz.muni.ics.kypo.training.api.enums.TDState;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.util.Objects;
 
 public class TrainingDefinitionInfoDTO {
 
     private Long id;
     private String title;
+    private TDState state;
 
     public Long getId() {
         return id;
@@ -23,18 +27,28 @@ public class TrainingDefinitionInfoDTO {
         this.title = title;
     }
 
+    @ApiModelProperty(value = "Current state of training definition.", example = "UNRELEASED")
+    public TDState getState() {
+        return state;
+    }
+
+    public void setState(TDState state) {
+        this.state = state;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TrainingDefinitionInfoDTO that = (TrainingDefinitionInfoDTO) o;
         return Objects.equals(id, that.id) &&
-                Objects.equals(title, that.title);
+                Objects.equals(title, that.title) &&
+                state == that.state;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title);
+        return Objects.hash(id, title, state);
     }
 
     @Override
@@ -42,6 +56,7 @@ public class TrainingDefinitionInfoDTO {
         return "TrainingDefinitionInfoDTO{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
+                ", state=" + state +
                 '}';
     }
 }
