e09b4fb76a1e188fae0db872b9eef0c631994917
Pavel Seda <441048@mail.muni.cz>
2020-01-23 13:29:08 +0100

    Upgrade security-commons version and remove developers from maven.

--- kypo2-rest-training/src/main/java/cz/muni/ics/kypo/training/rest/config/SpringBootRun.java
+++ kypo2-rest-training/src/main/java/cz/muni/ics/kypo/training/rest/config/SpringBootRun.java
@@ -9,41 +9,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Import;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-/**
- * <p>
- * To run with external property file add following to:
- * <p>
- * Eclipse example:
- *
- * <pre>
- * <code>
- *  Run Configuration -> tab: Arguments -> Program arguments
- * </code>
- * </pre>
- * </p>
- *
- * <pre>
- * <code>
- *  --path.to.config.file="C:/CSIRT/property-files/training.properties"
- * </code>
- * </pre>
- * <p>
- * Intellij idea example:
- *
- * <pre>
- *  <code>
- *   Run Configuration -> tab: Arguments -> Program arguments
- *  </code>
- * </pre>
- * </p>
- *
- * <pre>
- *  <code>
- *   --path.to.config.file="/etc/kypo2/training/application.properties"
- *  </code>
- * </pre>
- *
- */
 @SpringBootApplication(scanBasePackages = "cz.muni.ics.kypo.training.rest")
 @EnableSpringDataWebSupport
 @Import({WebConfigRestTraining.class, FacadeConfiguration.class, SwaggerConfig.class})
