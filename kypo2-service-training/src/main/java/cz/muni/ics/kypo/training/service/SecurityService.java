02e3126bb2dd68ee074786414562145ce54f02d2
SedaQ <pavelseda@email.cz>
2020-03-13 20:19:08 +0100

    add remove unused methods.

--- kypo2-service-training/src/main/java/cz/muni/ics/kypo/training/service/SecurityService.java
+++ kypo2-service-training/src/main/java/cz/muni/ics/kypo/training/service/SecurityService.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.google.gson.JsonObject;
-import cz.muni.ics.kypo.commons.security.enums.AuthenticatedUserOIDCItems;
 import cz.muni.ics.kypo.training.annotations.transactions.TransactionalRO;
 import cz.muni.ics.kypo.training.api.dto.UserRefDTO;
 import cz.muni.ics.kypo.training.enums.RoleTypeSecurity;
@@ -57,6 +55,7 @@ public class SecurityService {
         this.trainingRunRepository = trainingRunRepository;
         this.restTemplate = restTemplate;
     }
+
     public boolean isTraineeOfGivenTrainingRun(Long trainingRunId) {
         TrainingRun trainingRun = trainingRunRepository.findById(trainingRunId).orElseThrow(() -> new EntityNotFoundException(new EntityErrorDetail(TrainingRun.class, "id", trainingRunId.getClass(),
                 trainingRunId, "The necessary permissions are required for a resource.")));
@@ -73,16 +72,6 @@ public class SecurityService {
         TrainingDefinition trainingDefinition = trainingDefinitionRepository.findById(definitionId).orElseThrow(() -> new EntityNotFoundException(new EntityErrorDetail(TrainingDefinition.class,
                 "id", definitionId.getClass(), definitionId, "The necessary permissions are required for a resource.")));
         return trainingDefinition.getAuthors().stream().anyMatch(a -> a.getUserRefId().equals(getUserRefIdFromUserAndGroup()));
-    }
-
-    public boolean isInBetaTestingGroup(Long definitionId) {
-        TrainingDefinition trainingDefinition = trainingDefinitionRepository.findById(definitionId).orElseThrow(() -> new EntityNotFoundException(new EntityErrorDetail(TrainingDefinition.class,
-                "id", definitionId.getClass(), definitionId, "The necessary permissions are required for a resource.")));
-        if (trainingDefinition.getBetaTestingGroup() == null) {
-            throw new EntityNotFoundException(new EntityErrorDetail(TrainingDefinition.class,
-                    "id", definitionId.getClass(), definitionId, "The necessary permissions are required for a resource."));
-        }
-        return trainingDefinition.getBetaTestingGroup().getOrganizers().stream().anyMatch(o -> o.getUserRefId().equals(getUserRefIdFromUserAndGroup()));
     }
 
     public boolean isAdmin() {
@@ -109,36 +98,6 @@ public class SecurityService {
         return false;
     }
 
-    public String getSubOfLoggedInUser() {
-        OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
-        JsonObject credentials = (JsonObject) authentication.getUserAuthentication().getCredentials();
-        return credentials.get(AuthenticatedUserOIDCItems.SUB.getName()).getAsString();
-    }
-
-    public String getFullNameOfLoggedInUser() {
-        OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
-        JsonObject credentials = (JsonObject) authentication.getUserAuthentication().getCredentials();
-        return credentials.get(AuthenticatedUserOIDCItems.NAME.getName()).getAsString();
-    }
-
-    public String getGivenNameOfLoggedInUser() {
-        OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
-        JsonObject credentials = (JsonObject) authentication.getUserAuthentication().getCredentials();
-        return credentials.get(AuthenticatedUserOIDCItems.GIVEN_NAME.getName()).getAsString();
-    }
-
-    public String getFamilyNameOfLoggedInUser() {
-        OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
-        JsonObject credentials = (JsonObject) authentication.getUserAuthentication().getCredentials();
-        return credentials.get(AuthenticatedUserOIDCItems.FAMILY_NAME.getName()).getAsString();
-    }
-
-    public String getIssOfLoggedInUser() {
-        OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
-        JsonObject credentials = (JsonObject) authentication.getUserAuthentication().getCredentials();
-        return credentials.get(AuthenticatedUserOIDCItems.ISS.getName()).getAsString();
-    }
-
     public Long getUserRefIdFromUserAndGroup() {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
@@ -146,22 +105,6 @@ public class SecurityService {
             ResponseEntity<UserRefDTO> userInfoResponseEntity = restTemplate.exchange(userAndGroupURI + "/users/info", HttpMethod.GET, new HttpEntity<>(httpHeaders), UserRefDTO.class);
             UserRefDTO userRefDto = userInfoResponseEntity.getBody();
             return userRefDto.getUserRefId();
-        } catch (HttpClientErrorException ex) {
-            try {
-                throw new MicroserviceApiException("Error when calling UserAndGroup API to get info about logged in user.", convertJsonBytesToObject(ex.getResponseBodyAsString(), JavaApiError.class));
-            } catch (IOException ex1) {
-                throw new InternalServerErrorException("Unable to parse ApiError when calling UserAndGroup API");
-            }
-        }
-    }
-
-    public UserRefDTO getUserRefDTOFromUserAndGroup() {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        try {
-            ResponseEntity<UserRefDTO> userInfoResponseEntity = restTemplate.exchange(userAndGroupURI + "/users/info", HttpMethod.GET, new HttpEntity<>(httpHeaders), new ParameterizedTypeReference<UserRefDTO>() {
-            });
-            return userInfoResponseEntity.getBody();
         } catch (HttpClientErrorException ex) {
             try {
                 throw new MicroserviceApiException("Error when calling UserAndGroup API to get info about logged in user.", convertJsonBytesToObject(ex.getResponseBodyAsString(), JavaApiError.class));
@@ -191,10 +134,11 @@ public class SecurityService {
             }
         }
     }
+
     private static <T> T convertJsonBytesToObject(String object, Class<T> objectClass) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        mapper.registerModule( new JavaTimeModule()).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.registerModule(new JavaTimeModule()).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return mapper.readValue(object, objectClass);
     }
 
