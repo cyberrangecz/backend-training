e0610bc952c75548f244afbccd496b613ec34b3b
Dominik Pilar <xpilar@fi.muni.cz>
2021-06-17 10:08:44 +0200

    Changed format of the EMI answers in the archived instance.

---
+++ kypo2-api-training/src/main/java/cz/muni/ics/kypo/training/api/dto/archive/QuestionEMIAnswer.java
@@ -0,0 +1,44 @@
+package cz.muni.ics.kypo.training.api.dto.archive;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.models.auth.In;
+
+import java.util.Objects;
+
+public class QuestionEMIAnswer {
+
+    @JsonProperty("statementOrder")
+    private Integer statementOrder;
+    @JsonProperty("optionOrder")
+    private Integer optionOrder;
+
+    public Integer getStatementOrder() {
+        return statementOrder;
+    }
+
+    public void setStatementOrder(Integer statementOrder) {
+        this.statementOrder = statementOrder;
+    }
+
+    public Integer getOptionOrder() {
+        return optionOrder;
+    }
+
+    public void setOptionOrder(Integer optionOrder) {
+        this.optionOrder = optionOrder;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        QuestionEMIAnswer that = (QuestionEMIAnswer) o;
+        return getStatementOrder().equals(that.getStatementOrder()) &&
+                getOptionOrder().equals(that.getOptionOrder());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getStatementOrder(), getOptionOrder());
+    }
+}
