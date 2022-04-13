15ea5da712029a4786f31c9b3a0a21d928393f19
Dominik Pilár <445537@mail.muni.cz>
2022-04-13 17:04:34 +0200

    Resolve "Fix progress visualization data - remove hradcoded page size"

--- kypo-rest-training/src/main/java/cz/muni/ics/kypo/training/rest/controllers/VisualizationRestController.java
+++ kypo-rest-training/src/main/java/cz/muni/ics/kypo/training/rest/controllers/VisualizationRestController.java
@@ -370,8 +370,7 @@ public class VisualizationRestController {
             @ApiResponse(code = 500, message = "Unexpected condition was encountered.", response = ApiError.class)
     })
     @GetMapping(path = "/training-instances/{instanceId}/progress", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> getProgressVisualizations(@ApiParam(value = "Training instance ID", required = true)
-                                                                                     @PathVariable("instanceId") Long trainingInstanceId) {
+    public ResponseEntity<Object> getProgressVisualizations(@ApiParam(value = "Training instance ID", required = true) @PathVariable("instanceId") Long trainingInstanceId) {
         VisualizationProgressDTO visualizationProgressDTO = visualizationFacade.getProgressVisualization(trainingInstanceId);
         return ResponseEntity.ok(SquigglyUtils.stringify(objectMapper, visualizationProgressDTO));
     }
