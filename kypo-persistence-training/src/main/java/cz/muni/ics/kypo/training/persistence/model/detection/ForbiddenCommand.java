a80f3242f0bd86e7a58ab438c4a059d6ef41d721
xmajdan <445519@mail.muni.cz>
2024-01-21 17:13:50 +0100

    Add reference to cheating detection to forbidden command

--- kypo-persistence-training/src/main/java/cz/muni/ics/kypo/training/persistence/model/detection/ForbiddenCommand.java
+++ kypo-persistence-training/src/main/java/cz/muni/ics/kypo/training/persistence/model/detection/ForbiddenCommand.java
@@ -1,6 +1,7 @@
 package cz.muni.ics.kypo.training.persistence.model.detection;
 
 import cz.muni.ics.kypo.training.persistence.model.AbstractEntity;
+import cz.muni.ics.kypo.training.persistence.model.AssessmentLevel;
 import cz.muni.ics.kypo.training.persistence.model.enums.CommandType;
 
 import javax.persistence.*;
@@ -15,7 +16,9 @@ public class ForbiddenCommand extends AbstractEntity<Long> {
     private String command;
     @Column(name = "command_type", nullable = false)
     private CommandType type;
-
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cheating_detection_id")
+    private CheatingDetection cheatingDetection;
     public String getCommand() {
         return command;
     }
