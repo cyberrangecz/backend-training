1ba1c2ee84fd600b2a3850e45f11fa66e35bd6cd
Lukáš <445519@mail.muni.cz>
2024-04-09 15:03:28 +0200

    Add ordering of cheating detection

--- kypo-persistence-training/src/main/java/cz/muni/ics/kypo/training/persistence/model/detection/CheatingDetection.java
+++ kypo-persistence-training/src/main/java/cz/muni/ics/kypo/training/persistence/model/detection/CheatingDetection.java
@@ -18,7 +18,8 @@ import java.util.*;
         @NamedQuery(
                 name = "CheatingDetection.findAllByTrainingInstanceId",
                 query = "SELECT cd FROM CheatingDetection cd " +
-                        "WHERE cd.trainingInstanceId = :trainingInstanceId"
+                        "WHERE cd.trainingInstanceId = :trainingInstanceId +" +
+                        "ORDER BY cd.executedAt"
         ),
         @NamedQuery(
                 name = "CheatingDetection.findCheatingDetectionById",
