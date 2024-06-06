cf090b83851ac7c61566bbde6da8701365da7a32
Lukáš <445519@mail.muni.cz>
2024-06-06 10:51:16 +0200

    reorder migration elements

--- kypo-persistence-training/src/main/resources/db/migration/training/V19__db_cheating_detection_tables_adjustments.sql
+++ kypo-persistence-training/src/main/resources/db/migration/training/V19__db_cheating_detection_tables_adjustments.sql
@@ -1,26 +1,23 @@
-ALTER TABLE forbidden_command DROP CONSTRAINT forbidden_command_cheating_detection_id_fkey;
-ALTER TABLE forbidden_command ALTER COLUMN cheating_detection_id DROP NOT NULL;
-
-alter table detected_forbidden_command add column hostname varchar(255);
-alter table detected_forbidden_command add column occurred_at timestamp;
+alter table forbidden_command drop constraint forbidden_command_cheating_detection_id_fkey;
+alter table forbidden_command alter column cheating_detection_id drop not null;
 
 alter table abstract_detection_event add column level_order int4;
 
 alter table abstract_detection_event add column training_run_id int4;
-alter table forbidden_commands_detection_event drop column training_run_id;
 
-ALTER TABLE abstract_detection_event
-    ADD COLUMN participants text NOT NULL;
+alter table abstract_detection_event add column participants text not null;
 
-ALTER TABLE forbidden_commands_detection_event
-    ADD COLUMN training_run_id int8 NOT NULL,
-    ADD COLUMN command_count int8 NOT NULL,
-    ALTER COLUMN forbidden_commands DROP NOT NULL;
+alter table forbidden_commands_detection_event
+    add column command_count int8 not null,
+    alter column forbidden_commands drop not null;
 
-CREATE TABLE detected_forbidden_command (
-   id bigserial NOT NULL,
-    command varchar(255) NOT NULL,
-    command_type varchar(255) NOT NULL,
-    detection_event_id int8 NOT NULL,
-    PRIMARY KEY (id)
+create table detected_forbidden_command (
+   id bigserial not null,
+    command varchar(255) not null,
+    command_type varchar(255) not null,
+    detection_event_id int8 not null,
+    primary key (id)
 );
+
+alter table detected_forbidden_command add column hostname varchar(255);
+alter table detected_forbidden_command add column occurred_at timestamp;
