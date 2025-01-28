1e67f0ac85be15886faec243b5f75a241cad879f
Mário Murín <murin@cshub.cz>
2025-01-28 12:18:07 +0100

    Fix redundant import

--- api-training/src/main/java/cz/muni/ics/kypo/training/api/dto/imports/ImportTrainingDefinitionDTO.java
+++ api-training/src/main/java/cz/muni/ics/kypo/training/api/dto/imports/ImportTrainingDefinitionDTO.java
@@ -1,7 +1,6 @@
 package cz.muni.ics.kypo.training.api.dto.imports;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import cz.muni.ics.kypo.training.api.enums.TDState;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
