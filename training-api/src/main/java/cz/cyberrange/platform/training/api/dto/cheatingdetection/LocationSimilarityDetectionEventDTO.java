c5ac826290d9490b8ffd6dd169976bcb41a282a2
Mário Murín <murin@cshub.cz>
2026-07-24 22:16:53 +0200

    fix: include superclass fields in @EqualsAndHashCode via callSuper

--- training-api/src/main/java/cz/cyberrange/platform/training/api/dto/cheatingdetection/LocationSimilarityDetectionEventDTO.java
+++ training-api/src/main/java/cz/cyberrange/platform/training/api/dto/cheatingdetection/LocationSimilarityDetectionEventDTO.java
@@ -10,14 +10,19 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-@EqualsAndHashCode
-@ApiModel(value = "LocationSimilarityDetectionEventDTO", description = "A detection event of type Location Similarity.", parent = AbstractDetectionEventDTO.class)
+@EqualsAndHashCode(callSuper = true)
+@ApiModel(
+    value = "LocationSimilarityDetectionEventDTO",
+    description = "A detection event of type Location Similarity.",
+    parent = AbstractDetectionEventDTO.class)
 public class LocationSimilarityDetectionEventDTO extends AbstractDetectionEventDTO {
 
-    @ApiModelProperty(value = "Ip address of participant.", example = "1.1.1.1")
-    private String ipAddress;
-    @ApiModelProperty(value = "DNS of participant.", example = "dns.provider.cz")
-    private String dns;
-    @ApiModelProperty(value = "If the address is the same as deployment.", example = "false")
-    private boolean isAddressDeploy;
+  @ApiModelProperty(value = "Ip address of participant.", example = "1.1.1.1")
+  private String ipAddress;
+
+  @ApiModelProperty(value = "DNS of participant.", example = "dns.provider.cz")
+  private String dns;
+
+  @ApiModelProperty(value = "If the address is the same as deployment.", example = "false")
+  private boolean isAddressDeploy;
 }
--- training-api/src/main/java/cz/cyberrange/platform/training/api/dto/cheatingdetection/MinimalSolveTimeDetectionEventDTO.java
+++ training-api/src/main/java/cz/cyberrange/platform/training/api/dto/cheatingdetection/MinimalSolveTimeDetectionEventDTO.java
@@ -10,10 +10,13 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-@EqualsAndHashCode
-@ApiModel(value = "MinimalSolveTimeDetectionEventDTO", description = "A detection event of type Minimal Solve Time.", parent = AbstractDetectionEventDTO.class)
+@EqualsAndHashCode(callSuper = true)
+@ApiModel(
+    value = "MinimalSolveTimeDetectionEventDTO",
+    description = "A detection event of type Minimal Solve Time.",
+    parent = AbstractDetectionEventDTO.class)
 public class MinimalSolveTimeDetectionEventDTO extends AbstractDetectionEventDTO {
 
-    @ApiModelProperty(value = "Minimal time required to solve the level.", example = "1")
-    private Long minimalSolveTime;
+  @ApiModelProperty(value = "Minimal time required to solve the level.", example = "1")
+  private Long minimalSolveTime;
 }
