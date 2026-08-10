a9088b96e02d1e48c09dcbef0108e5c84e1e5228
Mário Murín <murin@cshub.cz>
2026-08-10 21:50:59 +0200

    fix: Definition ID mapping to null

--- training-service/src/main/java/cz/cyberrange/platform/training/service/mapping/mapstruct/TrainingRunMapper.java
+++ training-service/src/main/java/cz/cyberrange/platform/training/service/mapping/mapstruct/TrainingRunMapper.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 import java.util.Set;
 import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.data.domain.Page;
@@ -33,6 +34,10 @@ public interface TrainingRunMapper extends ParentMapper {
   TrainingRunDTO mapToDTO(TrainingRun entity);
 
   @Named("trainingRunToBasicDTO")
+  @Mapping(target = "trainingInstanceId", source = "trainingInstance.id")
+  @Mapping(target = "trainingDefinitionId", source = "trainingInstance.trainingDefinition.id")
+  @Mapping(target = "currentLevelId", source = "currentLevel.id")
+  @Mapping(target = "currentLevelOrder", source = "currentLevel.order")
   TrainingRunBasicDTO mapToBasicDTO(TrainingRun entity);
 
   @IterableMapping(qualifiedByName = "trainingRunToBasicDTO")
