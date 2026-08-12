f49065360ff5e67530957ebccb260a3f8eb2e662
Mário Murín <murin@cshub.cz>
2026-08-12 15:59:42 +0200

    fix: Extra Syslog envelope field crashing event response mapping

--- training-opensearch/src/main/java/cz/cyberrange/platform/training/opensearch/events/training/model/AbstractAuditPOJO.java
+++ training-opensearch/src/main/java/cz/cyberrange/platform/training/opensearch/events/training/model/AbstractAuditPOJO.java
@@ -2,6 +2,7 @@ package cz.cyberrange.platform.training.opensearch.events.training.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
@@ -28,6 +29,7 @@ import lombok.experimental.SuperBuilder;
             + " It provides 2 member variables 'timestamp' and 'type': 'timestamp' is generated based on current time; 'type'"
             + " is defined by the `TYPE` constant in each subclass")
 @JsonPropertyOrder({"type", "timestamp"})
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonTypeInfo(
     use = JsonTypeInfo.Id.NAME,
     include = JsonTypeInfo.As.EXISTING_PROPERTY,
